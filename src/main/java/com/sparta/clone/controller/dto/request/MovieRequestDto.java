d13b3f0b28f08ed89f2e531dcbd3d25ab687ad8c
prscsl <prscsl@naver.com>
2022-09-13 10:25:14 +0900

    무비 requestDto

---
+++ src/main/java/com/sparta/clone/controller/dto/request/MovieRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.clone.controller.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieRequestDto {
+
+    private String titleEng;
+}
